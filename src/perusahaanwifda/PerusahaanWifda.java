5fe35f71f92f0e77b136ab8b730541c52e0facc8
wifda <wifdamu@gmail.com>
2020-05-28 18:20:51 +0700

    first commit

---
+++ src/perusahaanwifda/PerusahaanWifda.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package perusahaanwifda;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author ASUS
+ */
+public class PerusahaanWifda {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new Login().setVisible(true);
+           
+    }
+    
+}
